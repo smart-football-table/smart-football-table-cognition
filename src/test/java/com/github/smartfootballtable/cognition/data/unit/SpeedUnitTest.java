603c068ba05f16447aa2e21ca901571b5502e7ef
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-06-14 13:49:24 +0200

    missing tests added

--- src/test/java/com/github/smartfootballtable/cognition/data/unit/SpeedUnitTest.java
+++ src/test/java/com/github/smartfootballtable/cognition/data/unit/SpeedUnitTest.java
@@ -2,7 +2,7 @@ package com.github.smartfootballtable.cognition.data.unit;
 
 import static com.github.smartfootballtable.cognition.data.unit.SpeedUnit.KMH;
 import static com.github.smartfootballtable.cognition.data.unit.SpeedUnit.MPH;
-import static com.github.smartfootballtable.cognition.data.unit.SpeedUnit.MPS;
+import static com.github.smartfootballtable.cognition.data.unit.SpeedUnit.*;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -13,15 +13,38 @@ class SpeedUnitTest {
 
 	@Test
 	void canConvertKmh() {
-		assertThat(KMH.convertTo(KMH, 1), is(1.0));
-		assertThat(KMH.convertTo(MPS, 1), closeTo(0.277778, 0.001));
-		assertThat(KMH.convertTo(MPH, 1), closeTo(0.621371, 0.001));
+		SpeedUnit source = KMH;
+		assertThat(source.convertTo(KMH, 1), is(1.0));
+		assertThat(source.convertTo(MPS, 1), closeTo(0.277778, 0.001));
+		assertThat(source.convertTo(IPM, 1), closeTo(656.1679, 0.001));
+		assertThat(source.convertTo(MPH, 1), closeTo(0.621371, 0.001));
 	}
 
 	@Test
 	void canConvertMps() {
-		assertThat(MPS.convertTo(MPS, 1), is(1.0));
-		assertThat(MPS.convertTo(KMH, 1), is(3.6));
+		SpeedUnit source = MPS;
+		assertThat(source.convertTo(KMH, 1), is(3.6));
+		assertThat(source.convertTo(MPS, 1), is(1.0));
+		assertThat(source.convertTo(IPM, 1), closeTo(2362.2047, 0.001));
+		assertThat(source.convertTo(MPH, 1), closeTo(2.23694, 0.001));
+	}
+
+	@Test
+	void canConvertIpm() {
+		SpeedUnit source = IPM;
+		assertThat(source.convertTo(KMH, 1), closeTo(0.0015240000000000002, 0.0001));
+		assertThat(source.convertTo(MPS, 1), closeTo(0.00042333, 0.001));
+		assertThat(source.convertTo(IPM, 1), is(1.0));
+		assertThat(source.convertTo(MPH, 1), closeTo(0.00095699, 0.001));
+	}
+
+	@Test
+	void canConvertMph() {
+		SpeedUnit source = MPH;
+		assertThat(source.convertTo(KMH, 1), closeTo(1.6093, 0.001));
+		assertThat(source.convertTo(MPS, 1), closeTo(0.4470, 0.001));
+		assertThat(source.convertTo(IPM, 1), is(1056.0));
+		assertThat(source.convertTo(MPH, 1), is(1.0));
 	}
 
 }
