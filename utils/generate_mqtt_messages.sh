dbc815e5f65f26637933e7f781eeaa5902f8a61f
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-04-05 14:32:08 +0200

    initial revision

---
+++ utils/generate_mqtt_messages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+[ -z "$HOST" ] && HOST=localhost
+
+publish() {
+	mosquitto_pub -h $HOST -t 'ball/position/rel' -m "$1"
+	sleep 0.04
+}
+
+while [ 1 ]
+do
+	type=$((RANDOM % 100))
+	if [ "$type" -ge 0 -a "$type" -lt 80 ]; then
+		times=$((RANDOM % 100))
+		for (( c=1; c<=$times; c++ ))
+		do  
+			x=$((RANDOM % 10000))
+			y=$((RANDOM % 10000))
+			publish "0.$x,0.$y"
+		done
+	fi
+	if [ "$type" -ge 80 -a "$type" -lt 95 ]; then
+		times=$((RANDOM % 100))
+		for (( c=1; c<=$times; c++ ))
+		do  
+			publish "-1.0,-1.0"
+		done
+	fi
+	if [ "$type" -ge 95 -a "$type" -lt 100 ]; then
+		publish "-1.0,-1.0"
+	fi
+
+done
