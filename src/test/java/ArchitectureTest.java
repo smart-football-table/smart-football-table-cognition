1d0c73181907643c3fa5af325e6d6d004b8667e8
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-08-23 16:30:46 +0200

    check detectors do not depend on each other

--- src/test/java/ArchitectureTest.java
+++ src/test/java/ArchitectureTest.java
@@ -1,14 +1,54 @@
+import static com.tngtech.archunit.lang.SimpleConditionEvent.violated;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+import com.github.smartfootballtable.cognition.detector.Detector;
+import com.tngtech.archunit.core.domain.Dependency;
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
 
 @AnalyzeClasses(packages = "com.github.smartfootballtable.cognition", importOptions = DoNotIncludeTests.class)
 class ArchitectureTest {
 
 	@ArchTest
 	ArchRule noCycle = slices().matching("com.github.smartfootballtable.cognition.(*)..").should().beFreeOfCycles();
+
+	@ArchTest
+	ArchRule detectorsMustNotDependOnEachOther = classes() //
+			.that().resideInAPackage("com.github.smartfootballtable.cognition.detector") //
+			.and().areTopLevelClasses() //
+			.should(notDependOnClassesInSamePackage()) //
+	;
+
+	private ArchCondition<JavaClass> notDependOnClassesInSamePackage() {
+		return new ArchCondition<JavaClass>("depend on classes in same package") {
+			@Override
+			public void check(JavaClass item, ConditionEvents events) {
+				for (Dependency dependency : item.getDirectDependenciesFromSelf()) {
+					if (samePackage(dependency.getOriginClass(), dependency.getTargetClass())
+							&& !dependency.getTargetClass().isNestedClass()
+							&& !implementing(Detector.class, dependency)) {
+						String message = String.format("Class %s has dependency %s", item.getFullName(), dependency);
+						events.add(violated(item, message));
+					}
+				}
+			}
+
+			private boolean samePackage(JavaClass class1, JavaClass class2) {
+				return class1.getPackage().equals(class2.getPackage());
+			}
+
+			private boolean implementing(Class<?> iface, Dependency dependency) {
+				return dependency.getTargetClass().isEquivalentTo(iface)
+						&& dependency.getOriginClass().getInterfaces().stream().anyMatch(c -> c.isEquivalentTo(iface));
+			}
+
+		};
+	}
 
 }
