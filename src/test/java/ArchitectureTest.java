eaea59093f1a6def89b93e52d305ba85c6b58580
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-08-23 17:37:29 +0200

    detectorsMustNotDependOnEachOther reworked

--- src/test/java/ArchitectureTest.java
+++ src/test/java/ArchitectureTest.java
@@ -1,7 +1,8 @@
 import static com.tngtech.archunit.lang.SimpleConditionEvent.violated;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+import com.github.smartfootballtable.cognition.SFTCognition;
 import com.github.smartfootballtable.cognition.detector.Detector;
 import com.tngtech.archunit.core.domain.Dependency;
 import com.tngtech.archunit.core.domain.JavaClass;
@@ -19,46 +20,30 @@ class ArchitectureTest {
 	ArchRule noCycle = slices().matching("com.github.smartfootballtable.cognition.(*)..").should().beFreeOfCycles();
 
 	@ArchTest
-	ArchRule detectorsMustNotDependOnEachOther = classes() //
-			.that().resideInAPackage("com.github.smartfootballtable.cognition.detector") //
+	ArchRule detectorsMustNotDependOnEachOther = noClasses() //
+			.that().resideInAnyPackage(detectorPackage()) //
 			.and().areTopLevelClasses() //
-			.should(notDependOnClassesInSamePackage()) //
+			.should().onlyBeAccessed().byClassesThat().resideInAPackage(detectorPackage()) //
 	;
 
 	@ArchTest
 	ArchRule detectorsShouldBeNamedAccordinglyAndResideInTheInterfacesPackage = classes() //
 			.that().implement(Detector.class) //
 			.should().haveSimpleNameEndingWith("Detector") //
-			.andShould().resideInAPackage(Detector.class.getPackage().getName());
+			.andShould().resideInAPackage(Detector.class.getPackage().getName()) //
+	;
+
+	@ArchTest
+	ArchRule classesInMainShouldNotReferedByOtherPackages = classes() //
+			.that().resideInAnyPackage(mainPackage()) //
+			.should().onlyBeAccessed().byClassesThat().resideInAPackage(mainPackage());
 
 	String detectorPackage() {
 		return "com.github.smartfootballtable.cognition.detector";
 	};
 
-	private ArchCondition<JavaClass> notDependOnClassesInSamePackage() {
-		return new ArchCondition<JavaClass>("not depend on classes in same package") {
-			@Override
-			public void check(JavaClass item, ConditionEvents events) {
-				for (Dependency dependency : item.getDirectDependenciesFromSelf()) {
-					if (samePackage(dependency.getOriginClass(), dependency.getTargetClass())
-							&& !dependency.getTargetClass().isNestedClass()
-							&& !implementing(Detector.class, dependency)) {
-						String message = String.format("Class %s has dependency %s", item.getFullName(), dependency);
-						events.add(violated(item, message));
-					}
-				}
-			}
-
-			private boolean samePackage(JavaClass class1, JavaClass class2) {
-				return class1.getPackage().equals(class2.getPackage());
-			}
-
-			private boolean implementing(Class<?> iface, Dependency dependency) {
-				return dependency.getTargetClass().isEquivalentTo(iface)
-						&& dependency.getOriginClass().getInterfaces().stream().anyMatch(c -> c.isEquivalentTo(iface));
-			}
-
-		};
+	String mainPackage() {
+		return "com.github.smartfootballtable.cognition.main";
 	}
 
 }
