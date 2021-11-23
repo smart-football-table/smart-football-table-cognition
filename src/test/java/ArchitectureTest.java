cc92860c710c0de9a4d8d4ead4b2dfb6a5c4fc07
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2021-11-23 17:51:56 +0100

    polishing

--- src/test/java/ArchitectureTest.java
+++ src/test/java/ArchitectureTest.java
@@ -1,17 +1,13 @@
-import static com.tngtech.archunit.lang.SimpleConditionEvent.violated;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
-import com.github.smartfootballtable.cognition.SFTCognition;
 import com.github.smartfootballtable.cognition.detector.Detector;
-import com.tngtech.archunit.core.domain.Dependency;
-import com.tngtech.archunit.core.domain.JavaClass;
+import com.github.smartfootballtable.cognition.main.Main;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.ConditionEvents;
 
 @AnalyzeClasses(packages = "com.github.smartfootballtable.cognition", importOptions = DoNotIncludeTests.class)
 class ArchitectureTest {
@@ -35,15 +31,15 @@ class ArchitectureTest {
 
 	@ArchTest
 	ArchRule classesInMainShouldNotReferedByOtherPackages = classes() //
-			.that().resideInAnyPackage(mainPackage()) //
-			.should().onlyBeAccessed().byClassesThat().resideInAPackage(mainPackage());
+			.that().resideInAnyPackage(packageOf(Main.class)) //
+			.should().onlyBeAccessed().byClassesThat().resideInAPackage(packageOf(Main.class));
 
 	String detectorPackage() {
 		return "com.github.smartfootballtable.cognition.detector";
 	};
 
-	String mainPackage() {
-		return "com.github.smartfootballtable.cognition.main";
+	String packageOf(Class<?> clazz) {
+		return clazz.getPackage().getName();
 	}
 
 }
